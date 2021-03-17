b7ba6aa735526e8cfadd7a0846e491d4856c8977
Luwangel <adrien.amoros@gmail.com>
2021-03-17 15:09:46 +0100

    Add a button to execute the code

--- packages/body-language/src/code/Code.jsx
+++ packages/body-language/src/code/Code.jsx
@@ -5,9 +5,18 @@ import { translate } from './translateToJS';
 
 export function Code({ instructions }) {
     const convertedCode = translate(instructions);
+
+    const handleExecute = () => {
+        console.log('Executing Ninja Code:');
+        eval(convertedCode);
+    };
+
     return (
         <div className="Code">
-            <code>{convertedCode}</code>
+            <code className="Code-code">{convertedCode}</code>
+            <button className="Code-button-try" onClick={handleExecute}>
+                Take the risk!
+            </button>
         </div>
     );
 }
