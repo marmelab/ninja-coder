0d6729c4e6e64de0c6b6910d393fb50ff9e74051
Luwangel <adrien.amoros@gmail.com>
2021-03-24 17:38:44 +0100

    Clean code

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,6 @@ import { NinjaContextProvider } from './NinjaContext';
 
 import { PosePredictor } from './pose/PosePredictor';
 import { Code } from './code/Code';
-import {
-    KEYWORD_CONSOLE,
-    KEYWORD_LOG,
-    SYNTAX_DOT,
-    SYNTAX_LEFT_BRACKET,
-    SYNTAX_RIGHT_BRACKET,
-    SYNTAX_STRING,
-    SYNTAX_SEMICOLON,
-    TEXT_COUCOU,
-} from './code/symbolsJS';
-
-// console.log('Hello World!');
-const instructions = [
-    KEYWORD_CONSOLE,
-    SYNTAX_DOT,
-    KEYWORD_LOG,
-    SYNTAX_LEFT_BRACKET,
-    SYNTAX_STRING,
-    TEXT_COUCOU,
-    SYNTAX_STRING,
-    SYNTAX_RIGHT_BRACKET,
-    SYNTAX_SEMICOLON,
-];
 
 function App() {
     return (
