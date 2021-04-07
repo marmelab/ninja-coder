03182a8122ad2867c43d3ce03e5c802d04c5163d
Luwangel <adrien.amoros@gmail.com>
2021-04-07 17:43:28 +0200

    Fix a bug when there is no model

--- src/pose/PosePredictor.jsx
+++ src/pose/PosePredictor.jsx
@@ -24,6 +24,9 @@ export const PosePredictor = () => {
     });
 
     const predict = async () => {
+        if (!model) {
+            return;
+        }
         // Prediction #1: run input through posenet
         // estimatePose can take in an image, video or canvas html element
         const { posenetOutput } = await model.estimatePose(webcam.canvas);
