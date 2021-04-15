f87c4594a7454a9a11472cceff5021affb6bff27
Guillaume BILLEY <guillaume.billey@pharmagest.com>
2021-04-15 12:32:06 +0200

    fix load juste once

--- src/NinjaContext.jsx
+++ src/NinjaContext.jsx
@@ -29,7 +29,7 @@ export const NinjaContextProvider = ({
 
             setModel(tmPoseModel);
         })();
-    });
+    }, []);
 
     const value = { model, predictions, pushPrediction, resetPredictions };
 
