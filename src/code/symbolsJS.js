1ebc1f98947ca08a20b9beef86ff7170153af796
Luwangel <adrien.amoros@gmail.com>
2021-04-21 10:03:13 +0200

    Clean the symbols

--- src/code/symbolsJS.js
+++ src/code/symbolsJS.js
@@ -1,11 +1,7 @@
 export const IDLE = 'IDLE';
-export const START = 'START';
-export const EXECUTE = 'EXECUTE';
 
 // Keywords
 
-export const KEYWORD_ALERT = 'ALERT';
-export const KEYWORD_ALERT_2 = 'ALERT_2';
 export const KEYWORD_CONSOLE = 'CONSOLE';
 export const KEYWORD_LOG = 'LOG';
 
@@ -15,16 +11,13 @@ export const SYNTAX_DOT = 'DOT';
 export const SYNTAX_LEFT_BRACKET = 'LEFT_BRACKET';
 export const SYNTAX_RIGHT_BRACKET = 'RIGHT_BRACKET';
 export const SYNTAX_SEMICOLON = 'SEMICOLON';
-export const SYNTAX_SEMICOLON_2 = 'SEMICOLON_2';
 export const SYNTAX_STRING = 'STRING';
-export const SYNTAX_STRING_2 = 'STRING_2';
 
 // Text
 
 export const TEXT_COUCOU = 'COUCOU';
 
 // Actions
-
 export const ACTION_ERASE = 'ERASE';
 export const ACTION_EXECUTE = 'EXECUTE';
-export const ACTION_EXECUTE_2 = 'EXECUTE_2';
+export const ACTION_START = 'START';
